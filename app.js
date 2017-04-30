6a03669cf5f447cd49f46c9044676b4540616d23
viveksharma619 <s.vivekblog@gmail.com>
2017-04-30 17:16:07 +0530

    Added Bootstrap and some input feilds

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var a = G$('Vivek', 'Sharma');
 
 $('#login').click(function(){
   //gets a  new object without new keyword
-  var loginGreetr = G$('Jane',' Doe');
+  var loginGreetr = G$($('#firstName').val(), $('#lastName').val());
   $('#logindiv').hide();
   //chainable methods 
   loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
