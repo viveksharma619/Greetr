6b8791245401c0fe9365a091297cfce703939afd
viveksharma619 <s.vivekblog@gmail.com>
2017-04-30 16:51:06 +0530

    Added HTML Greeting to the prototype

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//gets a  new object without new keyword
 var a = G$('Vivek', 'Sharma');
-a.greet();
-console.log(a);
+
+$('#login').click(function(){
+  //gets a  new object without new keyword
+  var loginGreetr = G$('Jane',' Doe');
+  $('#logindiv').hide();
+  //chainable methods 
+  loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+})
--- greetr.js
+++ greetr.js
@@ -1,23 +1,25 @@
-(function(global, $){
+;(function(global, $){
     "use strict";
     var versionNumber = '1.0.0';
     var supportedLanguages = ['es','en'];
+    //hidden within IIFE and not directly accessible
 
+    //greeting  
     var greetings = {
         en: 'Hello',
         es: 'Hola'
     };
-
+    //formal greeting
     var formalGreetings = {
         en: 'Greetings',
         es: 'Saludos'
     };
-
+    // log messages according to languages
     var logMessages = {
         en: 'Logged In',
         es: 'Inició sesión'
     }
-
+    // a new object
     var Greetr = function(firstName, lastName, language){
         return new Greetr.init(firstName, lastName, language);
     }
@@ -62,6 +64,26 @@
             this.language = lang;
             this.validate();
             return this;
+        },
+        HTMLGreeting: function(selector, formal){
+            if(!$){
+                throw 'jQuery instance not found!';
+            }
+
+            if(!selector){
+                throw 'Missing jQuery selector';
+            }
+            var msg;
+            if(formal){
+                msg = this.formalGreeting();
+            }
+            else{
+                msg = this.greeting();
+            }
+            $(selector).html(msg);
+
+            //method chaing
+            return this;
         }
     };
 
@@ -70,8 +92,10 @@
         self.firstName = firstName || 'John';
         self.lastName = lastName || 'Doe';
         self.language = language || 'en';
-    }
 
+        self.validate();
+    }
+    // trick borrowed from Jquery
     Greetr.init.prototype = Greetr.prototype;
 
     global.Greetr = global.G$ = Greetr;
